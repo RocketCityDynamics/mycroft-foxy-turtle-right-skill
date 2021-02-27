d5e88a9c7d12133d21cba0992708ab945443aa1f
Rocket City Dynamics <46632364+RocketCityDynamics@users.noreply.github.com>
2021-02-27 11:39:02 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ class MycroftFoxyTurtleRight(MycroftSkill):
     @intent_file_handler('right.turtle.foxy.mycroft.intent')
     def handle_right_turtle_foxy_mycroft(self, message):
         self.speak_dialog('right.turtle.foxy.mycroft')
-
+        subprocess.call(["ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist '{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: -1.5}}'"],shell=True)
 
 def create_skill():
     return MycroftFoxyTurtleRight()
